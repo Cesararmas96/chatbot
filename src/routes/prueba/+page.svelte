aec0eef3d493d50f90bde1f8b78e31efa79b2b3d
Cesar Armas <caal2096@gmail.com>
2024-04-26 02:29:31 -0400

    comments delete

--- src/routes/prueba/+page.svelte
+++ src/routes/prueba/+page.svelte
@@ -2,51 +2,10 @@
   import { onMount } from 'svelte';
   import axios from 'axios';
 
-  let query = ''; // Variable para almacenar la consulta del usuario
+  let query = '';
   let chatResponse = '';
-  // const apiUrl = 'https://ai-dev.trocdigital.net/api/v1/chat/Edu';
   let token = localStorage.getItem('token');
  
-
-  // const  handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   console.log(token)
-  //   try {
-  //     const response = await fetch(apiUrl, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Access-Control-Allow-Methods' : 'POST',
-  //         'Access-Control-Allow-Credentials' : 'true',
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Access-Control-Allow-Headers': '*',
-  //         'Authorization': `Bearer ${token}`,
-  //         'Connection' : 'keep-alive',
-  //         'Origin': 'https://ai-dev.trocdigital.net',
-  //         'Referer': 'https://ai-dev.trocdigital.net',
-  //         'Server': 'nginx/1.18.0',
-  //         'X-Frame-Options' : 'ALLOWALL',
-  //         'Content-Encoding' : 'gzip'
-
-
-  //       },
-  //       body: JSON.stringify({ query }) // Enviar la consulta en el cuerpo de la solicitud POST
-  //     });
-
-      
-  //      if (response.ok) {
-  //           const data = await response.json();
-  //           console.log(data);
-  //         } else {
-  //           console.error('Error al obtener el token:', response.statusText);
-  //         }
-
-  //     // const data = await response.json();
-  //     // chatResponse = JSON.stringify(data, null, 2);
-  //   } catch (error) {
-  //     console.error('There was a problem with the fetch operation:', error);
-  //   }
-  // }
-
 const handleSubmit = async (event) => {
   event.preventDefault();
   console.log(token);
@@ -64,7 +23,6 @@ const handleSubmit = async (event) => {
     if (response.status === 200) {
       const data = response.data;
       console.log(data);
-      // Realiza operaciones con los datos recibidos
     } else {
       console.error('Error al obtener la respuesta:', response.statusText);
     }

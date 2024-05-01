6ddb823ab344dfdcc9db5a60614153114b688ee4
Cesar Armas <caal2096@gmail.com>
2024-05-01 09:18:56 -0400

    api test bose

--- src/lib/helpers/commons.ts
+++ src/lib/helpers/commons.ts
@@ -1,6 +1,13 @@
 export enum ApiChatBot {
     trocers = "TROCers",
-    bose = "Bose",
+    bose = "BoseBot",
     askbrett = "AskBrett",
     oddie = "Oddie",
   }
+
+export enum NameChatBot {
+  trocers = "T-ROCers",
+  bose = "Bose",
+  askbrett = "AskBrett",
+  oddie = "Oddie",
+}

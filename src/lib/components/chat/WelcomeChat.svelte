dfda1e8fa2e713b2510aad29424d837dabee91d3
Cesar Armas <caal2096@gmail.com>
2024-05-22 16:38:35 -0400

    test 2 third-party

--- src/lib/components/chat/WelcomeChat.svelte
+++ src/lib/components/chat/WelcomeChat.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  // import { NameChatBot } from "../../helpers/commons";
+  import { NameChatBot } from "../../helpers/commons";
 
-  // import { page } from "$app/stores";
-  // let bot = $page.params.bot;
-  // export let botName;
+  import { page } from "$app/stores";
+  let bot = $page.params.bot;
+  export let botName;
 </script>
 
 <div class="flex flex-col justify-center items-center w-full h-full">
   <img src="/troc.png" width="150" alt="troc-logo" />
-  <!-- <h2 class="font-bold text-lg dark:text-white">Welcome Back to { NameChatBot[bot] !== undefined ? NameChatBot[bot] : botName  } Chatbot</h2> -->
+  <h2 class="font-bold text-lg dark:text-white">Welcome Back to { NameChatBot[bot] !== undefined ? NameChatBot[bot] : botName  } Chatbot</h2>
   
   <p class="text-gray-500">How can I help you today?</p>
 </div>

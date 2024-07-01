bcde125a9b01fda4543fd211a18ac7ef90f6d4bc
Cesar Armas <caal2096@gmail.com>
2024-07-01 19:54:37 -0400

    change svelte.config  adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,14 @@
-import adapter from '@sveltejs/adapter-node'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+  kit: {
+    adapter: adapter({
+      out: 'build' // El directorio de salida para el adaptador Node
+    })
+  },
+  preprocess: vitePreprocess()
+};
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		serviceWorker: {
-			register: false
-		}
-	}
-}
-
-export default config
+export default config;

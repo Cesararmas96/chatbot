ba5fe93cd94a3a03806205184c753d43409aff90
Cesar Armas <caal2096@gmail.com>
2024-07-01 22:06:58 -0400

    test

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
+import corsProxy from './src/lib/corsProxy';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		vite: {
+			server: {
+			  // Configura middleware para todas las solicitudes
+			  middleware: [
+				corsProxy,
+			  ],
+			},
+		  },
 	},
 	preprocess: [
 		preprocess({

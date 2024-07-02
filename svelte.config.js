0de7be20de51bf45e9d0e3e2d8d5de597239e51c
Cesar Armas <caal2096@gmail.com>
2024-07-01 22:21:25 -0400

    config aws svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,27 @@
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-		mdsvex(mdsvexConfig)
+  kit: {
+    // Utiliza el adaptador de AWS
+    adapter: adapter({
+      // Configura el directorio de salida
+      fallback: 'index.html'
+    })
+  },
+  preprocess: [
+    preprocess({
+      postcss: true
+    }),
+    mdsvex(mdsvexConfig)
+  ],
 
-	],
-
-	extensions: ['.svelte', ...mdsvexConfig.extensions]
-
-
-
+  extensions: ['.svelte', ...mdsvexConfig.extensions]
 };
 
 export default config;

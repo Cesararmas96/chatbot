ffc9f99e01fc8db5ffaaed9800b37496bac50f4f
Cesar Armas <caal2096@gmail.com>
2024-07-01 20:34:55 -0400

    svelte.config adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      out: 'build' // El directorio de salida para el adaptador Node
-    })
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };

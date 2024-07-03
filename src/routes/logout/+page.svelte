27f9d6f4c808dd811f4a2bc0eaa5ffcd4106d238
Cesar Armas <caal2096@gmail.com>
2024-07-03 14:51:52 -0400

    Update logout

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+   import { redirect } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ cookies }) => {
+  throw redirect(302, "/");
+};
+
+export const actions: Actions = {
+  default({ cookies }) {
+    cookies.delete("_session1", { path: "/" });
+    cookies.delete("_session2", { path: "/" });
+    cookies.delete("_session3", { path: "/" });
+
+    throw redirect(302, "/auth");
+  },
+};
+
+  </script>
+  
+  <p>Logging out...</p>
+  

ec2eebc5d30d1cf9a024556f3fd7677902f3bb83
Cesar Armas <caal2096@gmail.com>
2024-07-03 15:36:35 -0400

    page server add

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { redirect } from '@sveltejs/kit'
+    import type { Actions, PageServerLoad } from './$types'
+    
+    export const load: PageServerLoad = async ({ cookies }) => {
+        cookies.delete('_session1', { path: "/" })
+        cookies.delete('_session2', { path: "/" })
+        cookies.delete('_session3', { path: "/" })
+    
+    
+        throw redirect(302, '/auth')
+    }
+    
+    export const actions: Actions = {
+        default({ cookies }) {
+            cookies.delete('_session1', { path: "/" })
+            cookies.delete('_session2', { path: "/" })
+            cookies.delete('_session3', { path: "/" })
+        
+    
+            throw redirect(302, '/auth')
+        }
+    }
+    
+      </script>
+      
+      <p>Logging out</p>
+      

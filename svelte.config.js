f64835ef587badc7f5e4cc99928ba17a11ff43e9
Cesar Armas <caal2096@gmail.com>
2024-07-20 23:56:59 -0400

    change dev

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-netlify';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 // import adapter from '@sveltejs/adapter-node';

3cec427c5fa4c2aecf9215da427d6306e2a4c049
Cesar Armas <caal2096@gmail.com>
2024-08-16 14:27:14 -0400

    build prod

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import mdsvexConfig from './mdsvex.config.js';
 // Adaptar https://navai.trocdigital.net/
 // import adapter from '@sveltejs/adapter-netlify';
 
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 

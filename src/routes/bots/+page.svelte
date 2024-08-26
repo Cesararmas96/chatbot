b2409e095c26a7ec28b02a1d54d767d4dcb1d16c
Cesar Armas <caal2096@gmail.com>
2024-08-26 14:21:14 -0400

    part 1 add shadcn page bots

--- src/routes/bots/+page.svelte
+++ src/routes/bots/+page.svelte
@@ -1,5 +1,7 @@
-<script lang="ts">
-	import Card from '$lib/components/home/Card.svelte'
+<script>
+	import { onMount } from 'svelte'
+	import { getApiData } from '$lib/services/getData'
+	import * as Card from '$lib/components/ui/card/index.js'
 	import { storeBots } from '$lib/stores/bots'
 
 	export let data
@@ -7,22 +9,20 @@
 </script>
 
 <ul class="grid grid-cols-2 gap-4 p-4 sm:gap-5 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
-	<div
-		class="flex justify-center animate__animated animate__zoomIn card cursor-pointer p-6 hover:-translate-y-1 hover:scale-100 hover:shadow-lg border border-gray-500 dark:bg-gray-700 rounded-md"
-	>
-		<a href="/bots/new" class="dark:text-gray-200">
-			<img
-				src="/images/bots/add.png"
-				alt="img-add-logo"
-				class="h-auto w-full rounded-md object-cover object-center img-logo"
-			/>
-
-			<div class="mb-2 mt-6">
-				<h3 class="tracki text-center text-lg font-semibold uppercase">Create Bot</h3>
-			</div>
-		</a>
-	</div>
 	{#each $storeBots as bot}
-		<Card namebot={bot.name} name={bot.name} />
+		<a href="/{bot.name.toLowerCase()}" class="dark:text-gray-200">
+			<Card.Root class="flex flex-col justify-center items-center">
+				<Card.Header>
+					<Card.Title tag="h6">{bot.name}</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<img
+						src="/images/bots/{bot.name.toLowerCase()}.png"
+						alt="img-{name}-logo"
+						class="h-auto w-full rounded-md object-cover object-center img-logo"
+					/>
+				</Card.Content>
+			</Card.Root>
+		</a>
 	{/each}
 </ul>
